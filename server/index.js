02f00a34b32f4ad80539961f2f9d22305b6661da
Harshvardhan <132637309+Harshvardhan32@users.noreply.github.com>
2024-04-28 15:12:01 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin: "http://localhost:3000",
+		origin: "https://studynotion-edtech-plateform.netlify.app",
 		credentials: true,
 	})
 );
