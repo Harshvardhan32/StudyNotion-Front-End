7e46e01a92bcc319020e7b5cbf158d86d82b92b7
Harshvardhan32 <harshvardhanmaurya73073@gmail.com>
2024-05-10 23:06:11 +0530

    bug fixed

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin: ["http://localhost:3000", 'https://studynotion-edtech-plateform.netlify.app'],
+		origin: ['https://studynotion-edtech-plateform.netlify.app', 'http://localhost:3000'],
 		credentials: true,
 	})
 );
